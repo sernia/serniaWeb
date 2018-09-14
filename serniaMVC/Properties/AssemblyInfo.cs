5754ad192ede54123a45789f7595cdad5c4a15f6
sernia <sernia@gmail.com>
2018-09-14 19:37:19 +0900

    サイト名を修正

--- serniaMVC/Properties/AssemblyInfo.cs
+++ serniaMVC/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は、以下の属性セットによって 
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("serniaMVC")]
+[assembly: AssemblyTitle("serniaSite")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
